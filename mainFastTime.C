6aa0eb7372c01b88d019cdd3328fb87dbb0611e6
arabella <arabella.martelli@cern.ch>
2019-10-08 10:09:50 +0200

    clean code

--- mainFastTime.C
+++ mainFastTime.C
@@ -5,24 +5,30 @@
 int main(){
 
   std::cout << " >>> in mainFastTime timing analysis " << std::endl;
-  const int nfiles = 8;
-  int energyNames[nfiles] = {30, 50, 80, 100, 150, 200, 250, 300};
-  //  int energyNames[nfiles] = {20};
-  
+  const int nfiles = 9;
+  int energyNames[nfiles] = {20, 30, 50, 80, 100, 150, 200, 250, 300};
+
+  //better to stick to realistic sim with Angle
   string path = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_october/offline_analysis/sim_ntuples/v2/withMCP/EMN/Angle/";
+  //  string path = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_october/offline_analysis/sim_ntuples/v2/withMCP/EMN/noAngle/";
 
   //int StopAtEvent=10000;
   int StopAtEvent=-1;
   //  int StopAtEvent=10;
   int ReportEvery=1000;
-  double mipCut = 1.0;
+  double mipCut = 15.0;
   
   
   for(int i=0; i<nfiles; i++){
     //ntuple_sim_config22_pdgID11_beamMomentum20_listFTFP_BERT_EMN.root
     string fname = Form("%sntuple_sim_config22_pdgID11_beamMomentum%d_listFTFP_BERT_EMN.root",path.c_str(),energyNames[i]);
-    
-    string outFname = Form("/eos/cms/store/group/upgrade/HGCAL/simulation/2018/Oct/V2/timingStudies/amartellTests/histo_%d.root",energyNames[i]);
+
+    //your output path
+    string outFname = Form("/eos/cms/store/group/upgrade/HGCAL/simulation/2018/Oct/V2/timingStudies/histo_%d.root",energyNames[i]);    
+    //string outFname = Form("/eos/cms/store/group/upgrade/HGCAL/simulation/2018/Oct/V2/timingStudies/amartellTests/histoTest_%d.root",energyNames[i]);
+
+    //string outFname = Form("/eos/cms/store/group/upgrade/HGCAL/simulation/2018/Oct/V2/timingStudies/amartellTests/histoSmearingOK_%d.root",energyNames[i]);
+    //  string outFname = Form("/eos/cms/store/group/upgrade/HGCAL/simulation/2018/Oct/V2/timingStudies/amartellTests/noTime_histo_%d.root",energyNames[i]);
 
     cout<<"======Looping over "<<fname<<endl;
 
